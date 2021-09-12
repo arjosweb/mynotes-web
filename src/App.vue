7715d4a60cee540526eb0605558a7ee565731c74
Artur <arjos.eu@outlook.com>
2021-09-12 16:24:34 -0300

    Testing Vercel Deploy

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire">
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>My Notes</v-toolbar-title>
     </v-app-bar>
 
     <v-navigation-drawer
